3068bdc0dbf5a683e8b02cc8743c76ac356ca8ac
Alix Lourme <alix.lourme@gmail.com>
2018-10-10 19:41:23 +0200

    Fix deployment process (on dev repository)

--- bintray-before-deploy.sh
+++ bintray-before-deploy.sh
@@ -5,7 +5,7 @@
 
 FILE_DESCRIPTOR="bintray-descriptor.json";
 
-VERSION=`cat target/classes/META-INF/maven/org.blondin/mpg-coach-bot/pom.properties | grep "^version" | cut -d= -f2`;
+VERSION=`cat target/maven-archiver/pom.properties | grep "^version" | cut -d= -f2`;
 echo "Project version: $VERSION";
 VERSION_SHORT=`echo "$VERSION" | sed 's/SNAPSHOT/SNAP/'`;
 echo "Version short  : $VERSION_SHORT";
@@ -13,7 +13,9 @@ echo "Version short  : $VERSION_SHORT";
 echo "Updating version in '$FILE_DESCRIPTOR' file";
 sed -i -- "s/VERSION_TO_UPDATE/$VERSION_SHORT/g" $FILE_DESCRIPTOR
 
-if [[ "$VERSION_SHORT" =~ "SNAP" ]]; then
+IS_SNAP=`echo $VERSION_SHORT | grep SNAP`;
+
+if [ "$IS_SNAP" ]; then
     echo "Updating repository (snapshot) in '$FILE_DESCRIPTOR' file";
     sed -i -- "s/generic/generic-dev/g" $FILE_DESCRIPTOR
 fi
