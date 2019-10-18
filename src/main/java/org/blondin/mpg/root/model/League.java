5424a1a0e507b92ad71f8cdac34ace5a4149a9dd
Alix Lourme <alix.lourme@gmail.com>
2019-10-19 01:24:09 +0200

    Fix #129 : Remove logic from jackson bean

--- src/main/java/org/blondin/mpg/root/model/League.java
+++ src/main/java/org/blondin/mpg/root/model/League.java
@@ -18,45 +18,34 @@ public class League {
     private League subLeague;
 
     public String getId() {
-        if (this.isMasterLeague) {
-            return subLeague.getId();
-        }
         return id;
     }
 
     public String getName() {
-        if (this.isMasterLeague) {
-            return subLeague.getName();
-        }
         return name;
     }
 
     public ChampionshipType getChampionship() {
-        if (this.isMasterLeague) {
-            return subLeague.getChampionship();
-        }
         return championship;
     }
 
     public LeagueStatus getLeagueStatus() {
-        if (this.isMasterLeague) {
-            return subLeague.getLeagueStatus();
-        }
         return leagueStatus;
     }
 
     public Mode getMode() {
-        if (this.isMasterLeague) {
-            return subLeague.getMode();
-        }
         return mode;
     }
 
     public int getTeamStatus() {
-        if (this.isMasterLeague) {
-            return subLeague.getTeamStatus();
-        }
         return teamStatus;
     }
 
+    public boolean isMasterLeague() {
+        return isMasterLeague;
+    }
+
+    public League getSubLeague() {
+        return subLeague;
+    }
 }
