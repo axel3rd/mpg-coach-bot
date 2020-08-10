396a6d106d05216d5295adb883e6db8c613acfa4
Alix Lourme <alix.lourme@gmail.com>
2020-08-10 22:36:04 +0200

    Fix #160 : Upgrade MPG client version header - Tryfix "Connection reset"

--- src/main/java/org/blondin/mpg/root/MpgClient.java
+++ src/main/java/org/blondin/mpg/root/MpgClient.java
@@ -29,7 +29,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
  */
 public class MpgClient extends AbstractClient {
 
-    public static final String MPG_CLIENT_VERSION = "5.8.1";
+    public static final String MPG_CLIENT_VERSION = "6.2.0";
 
     private static final String PATH_LEAGUE = "league/";
 
