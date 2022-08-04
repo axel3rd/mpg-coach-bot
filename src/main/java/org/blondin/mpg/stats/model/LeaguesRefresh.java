479bfcb24f6a14238ca208df2756214821a90562
Alix Lourme <alix.lourme@gmail.com>
2022-08-04 19:43:37 +0200

    Fix #235 : mpgstats API format change on leagues refresh

--- src/main/java/org/blondin/mpg/stats/model/LeaguesRefresh.java
+++ src/main/java/org/blondin/mpg/stats/model/LeaguesRefresh.java
@@ -2,8 +2,10 @@ package org.blondin.mpg.stats.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LeaguesRefresh {
 
     @JsonProperty("1")
