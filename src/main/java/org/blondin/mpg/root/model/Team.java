f64ef97e17d664e98fe15baf101ec8d299b09fd4
Alix Lourme <alix.lourme@gmail.com>
2025-01-12 18:09:13 +0100

    Fix #270 : Exception in parsing Team (due to Bonuses change)

--- src/main/java/org/blondin/mpg/root/model/Team.java
+++ src/main/java/org/blondin/mpg/root/model/Team.java
@@ -1,7 +1,9 @@
 package org.blondin.mpg.root.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -12,14 +14,47 @@ public class Team {
     private int budget;
     private Map<String, Player> squad;
     private List<Player> bids;
-    private Map<String, Integer> bonuses;
+    private Map<String, Object> bonuses;
 
+    /**
+     * Since January 2025 Bonuses become not only a list of "key:int".<br/>
+     * But a new key with (wrong?) sum was introduced, with linked "key:int" list.<br/>
+     * So hack was found to keep previous Map<String, Integer> getBonuses() method sign.<br/>
+     * "bonuses": {<br/>
+     * "10": {<br/>
+     * "blockTacticalSubs": 1,<br/>
+     * "boostAllPlayers": 1,<br/>
+     * "boostOnePlayer": 3,<br/>
+     * "fourStrikers": 1,<br/>
+     * "mirror": 1,<br/>
+     * "nerfGoalkeeper": 2,<br/>
+     * "removeGoal": 1,<br/>
+     * "removeRandomPlayer": 1<br/>
+     * },<br/>
+     * "blockTacticalSubs": 1,<br/>
+     * "boostAllPlayers": 1,<br/>
+     * "boostOnePlayer": 3,<br/>
+     * "fourStrikers": 1,<br/>
+     * "mirror": 1,<br/>
+     * "nerfGoalkeeper": 2,<br/>
+     * "removeGoal": 1,<br/>
+     * "removeRandomPlayer": 1<br/>
+     * },
+     * 
+     * @return Map of Bonuses
+     */
     public Map<String, Integer> getBonuses() {
-        return bonuses;
+        Map<String, Integer> b = new HashMap<>();
+        for (Entry<String, Object> e : bonuses.entrySet()) {
+            if (e.getValue() instanceof Integer) {
+                b.put(e.getKey(), (Integer) e.getValue());
+            }
+        }
+        return b;
     }
 
     public int getBonusesNumber() {
-        return bonuses.values().stream().reduce(0, Integer::sum);
+        return getBonuses().values().stream().reduce(0, Integer::sum);
     }
 
     public String getName() {
